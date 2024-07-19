85b81fc865f16d49db3cd56d4342667d74e9cc27
Anurag Sen <77142556+AnuragCsharp@users.noreply.github.com>
2024-07-19 12:38:18 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  
-  distDir: 'nextbuild' // Set the custom output directory here
+  distDir: 'nextbuild', // Set the custom output directory here
+  basePath: '/dualmindgithubpage.io', // The repository name on GitHub Pages
+  assetPrefix: '/dualmindgithubpage.io/',
 };
 
 export default nextConfig;
